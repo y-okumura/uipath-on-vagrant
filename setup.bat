8ac103cf1910556c7245a04153dbd7abe01f7593
Y Okumura <y-okumura@nichiwa-system.co.jp>
2018-09-14 20:27:26 +0900

    vagrantを入れるまで書くだけ書いてみた。動くかどうかは知らない

--- setup.bat
+++ setup.bat
@@ -1,1 +1,4 @@
 DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')"
+
+scoop install vagrant
