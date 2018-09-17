dea6e5c5ce792cf80b68aee46477f519f548c79b
Y Okumura <y-okumura@nichiwa-system.co.jp>
2018-09-18 01:44:47 +0900

    Zipファイルからboxファイルの生成。
    zipとboxで二重に持ってしまうことになるが、中間ファイルは無しで処理できた。
    ネットワーク上のストリームから直接エントリを抜けばboxの容量だけで作業可能だが、ダウンロード中の切断などに耐えられなくなるためいったんイメージのzipを保存してから作業する。

--- buildbox.ps1
+++ buildbox.ps1
@@ -1,6 +1,9 @@
 # https://developer.microsoft.com/ja-jp/windows/downloads/virtual-machines のダウンロードリンクのURL
 $imageUrl = "https://aka.ms/windev_VM_hyperv"
 
+# 作業ルート。この配下にworkやdestフォルダを作って作業する
+$root = $PSScriptRoot
+
 #　生成するboxファイルのファイル名
 $boxFile = "windows10.box"
 
@@ -10,12 +13,59 @@ $metadata = @{
 }
 
 
-$zipPath = "$PSScriptRoot\box.zip"
+$workDir = "$root\work"
+$destDir = "$root\dest"
+$imageZip = "$workDir\image.zip"
 
-# Invoke-WebRequestはスリープすると止まるがbitsadminは継続するので、大きなファイルはbitsadminが良さそう
-bitsadmin.exe /Transfer getWindows10Image /Download $imageUrl $zipPath
+# imageが未ダウンロードならダウンロードする
+if (-not (Test-Path $imageZip)) {
+    mkdir $workDir
+    # Invoke-WebRequestはスリープすると止まるがbitsadminは継続するので、大きなファイルはbitsadminが良さそう
+    bitsadmin.exe /Transfer getWindows10Image /Download $imageUrl $imageZip
+}
 
-# TODO: zipPathのWinDev1808Eval\Virtual MachinesとWinDev1808Eval\Virtual Hard Disksをトップに移して
-# metadata.jsonを加えて$boxFileに改名
+# fixme: mkdir -Forceで常に空で作ってほしいけど、すでに有るとなぜかそのままになってしまう
+Remove-Item $destDir -Recurse -Force
+mkdir $destDir -Force
 
-Rename-Item -Path $zipPath -NewName $boxFile
+# boxファイル（zipファイル）の作成
+Add-Type -AssemblyName System.IO.Compression
+$imageArchive = New-Object IO.Compression.ZipArchive(New-Object IO.FileStream($imageZip, [IO.FileMode]::Open))
+try {
+    $boxZip = "$destDir\box.zip"
+
+    $boxArchive = New-Object IO.Compression.ZipArchive(
+        (New-Object IO.FileStream($boxZip, [IO.FileMode]::Create)),
+        [IO.Compression.ZipArchiveMode]::Create)
+    try {
+        # イメージファイルのトップディレクトリの中身を一階層上げてzip直下にコピー
+        $imageArchive.Entries | Where-Object {
+            -not $_.FullName.EndsWith('/')
+        } | ForEach-Object {
+            $newEntryPath = $_.FullName.Substring($_.FullName.indexOf('/') + 1)
+            $in = $_.Open()
+            $out = $boxArchive.CreateEntry($newEntryPath).Open()
+            try {
+                $in.CopyTo($out)
+            } finally {
+                $out.Close()
+                $in.Close()
+            }
+        }
+
+        # メタデータファイルをboxファイル直下に作成
+        $writer = New-Object IO.StreamWriter($boxArchive.CreateEntry("metadata.json").Open())
+        try {
+            $metadata | ConvertTo-Json | ForEach-Object { $writer.WriteLine($_) }
+        } finally {
+            $writer.Close()
+        }
+    } finally {
+        $boxArchive.dispose()
+    }
+
+    # 作成中は拡張子「.zip」でないとzip操作ができないので、最後に拡張子を「.box」に変更
+    Rename-Item -Path $boxZip -NewName $boxFile
+} finally {
+    $imageArchive.Dispose()
+}
