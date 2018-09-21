6cdb7dcff8cb0032bff16ae71ddf2fbba010ce31
Y Okumura <y-okumura@nichiwa-system.co.jp>
2018-09-21 14:16:00 +0900

    buildbox時にディレクトリがなければ作成するよう修正

--- buildbox.ps1
+++ buildbox.ps1
@@ -1,12 +1,19 @@
 # https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/ のダウンロードリンクのURL
 $imageUrl = "https://az792536.vo.msecnd.net/vms/VMBuild_20180425/Vagrant/MSEdge/MSEdge.Win10.Vagrant.zip"
 
-$zipPath = "$PSScriptRoot\work\box.zip"
-$destPath = "$PSScriptRoot\dest\"
+$workDir = Join-Path $PSScriptRoot 'work\'
+$destDir = Join-Path $PSScriptRoot 'dest\'
+$zipPath = Join-Path $workDir 'box.zip'
 
 if (-not (Test-Path $zipPath)) {
+    if (-not (Test-Path $workDir)) {
+        mkdir $workDir
+    }
     # Invoke-WebRequestはスリープすると止まるがbitsadminは継続するので、大きなファイルはbitsadminが良さそう
     bitsadmin.exe /Transfer getWindows10Image /Download /Priority FOREGROUND $imageUrl $zipPath
 }
 
-Expand-Archive $zipPath $destPath
+if (-not (Test-Path $destDir)) {
+    mkdir $destDir
+}
+Expand-Archive $zipPath $destDir
